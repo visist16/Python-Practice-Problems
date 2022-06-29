730b96ad74813b57ee4af53619549a23e6bb0028
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-06-29 22:49:08 +0530

    Add files via upload

---
+++ Program5.py
@@ -0,0 +1,34 @@
+#program to check whether a number is armstrong
+
+n = int(input('Enter a number'))
+originalValue = n
+#153
+cube = 0
+while(n>0):
+    num = n%10
+    cube = cube + pow(num,3)
+    n = n//10
+    print(n)
+    
+print(cube)
+if(originalValue==cube):
+    print('The n value is Armstrong')
+else:
+    print('The n value is not armstrong')
+    
+'''
+n = int(input('Enter a number'))
+#153
+toString = str(n)
+sum = 0
+for i in toString:
+    #sum = sum + int(i)*int(i)*int(i)
+    #sum = sum + (int(i)**3)
+    sum = sum + pow(int(i),3)
+    
+if(n==sum):
+    print('it is armstrong')
+else:
+    print(
+    'it is not armstrong')'''
+    
