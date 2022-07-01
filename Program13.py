f319f00e1d48397ed188f5f13866a544502e8d56
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-07-01 22:36:16 +0530

    Add files via upload

---
+++ Program13.py
@@ -0,0 +1,9 @@
+ #program to check if two strings are anagrams
+name1 = input('Enter name 1: ')
+name2 = input('Enter name 2: ')
+n1 = sorted(name1)
+n2 = sorted(name2)
+if (n1==n2):
+    print('The Entered words are anagrams.')
+else:
+    print('The Entered words are not anagrams.')
