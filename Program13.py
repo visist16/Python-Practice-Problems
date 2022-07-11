5c3a2bab048c1d9ba759ee61166413ecc9737a7f
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-07-11 21:07:52 +0530

    Update Program13.py

--- Program13.py
+++ Program13.py
@@ -1,6 +1,7 @@
  #program to check if two strings are anagrams
 name1 = input('Enter name 1: ')
 name2 = input('Enter name 2: ')
+#Here sort function is used
 n1 = sorted(name1)
 n2 = sorted(name2)
 if (n1==n2):
